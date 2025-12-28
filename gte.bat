a5723db9302d98fd4a3e31c913cfe58227763a1d
thrashem <thrash@thrashem.com>
2025-12-29 07:41:18 +0900

    add gte.bat

---
+++ gte.bat
@@ -0,0 +1,3 @@
+@echo off
+chcp 65001 >nul
+curl -s -G "https://translate.googleapis.com/translate_a/single" --data-urlencode "client=gtx" --data-urlencode "sl=auto" --data-urlencode "tl=en" --data-urlencode "dt=t" --data-urlencode "q=%*" | "C:\Program Files\Git\usr\bin\cut.exe" -d'"' -f2
