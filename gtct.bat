f1e4b0e94dcb2090830a86edbdbc24da53683fc1
thrashem <thrash@thrashem.com>
2026-05-20 01:37:00 +0900

    update gt*.bat

---
+++ gtct.bat
@@ -0,0 +1,3 @@
+@echo off
+chcp 65001 >nul
+"%SystemRoot%\System32\curl.exe" -s -G "https://translate.googleapis.com/translate_a/single" --data-urlencode "client=gtx" --data-urlencode "sl=auto" --data-urlencode "tl=zh-TW" --data-urlencode "dt=t" --data-urlencode "q=%~1" | "C:\Program Files\Git\usr\bin\cut.exe" -d'"' -f2
